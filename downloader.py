71e7ef72b03c6ce6eb9256609dcd8756df751712
Borealin <308704199deniel@gmail.com>
2021-01-15 10:33:24 +0800

    [ADD] add some function

--- downloader.py
+++ downloader.py
@@ -4,6 +4,7 @@ import requests
 import json
 import re
 import os
+import sys
 
 mobile_header = {
     "Origin": "file://",
@@ -28,6 +29,8 @@ course_main_page_url = "https://courses.zju.edu.cn/user/courses"
 courseware_list_url = "https://courses.zju.edu.cn/api/course/{}/coursewares?page=1&page_size=1000"
 
 ref_url = "https://courses.zju.edu.cn/api/uploads/reference/{}/blob"
+
+preview_url = "https://courses.zju.edu.cn/api/uploads/reference/document/{}/url?preview=true"
 
 user_id_pattern = "span id=\"userId\" data-id=\"(.+)\" value"
 
@@ -68,8 +71,19 @@ def get_download_list(s, course_id):
         if 'uploads' in courseware:
             if isinstance(courseware['uploads'], list):
                 for upload in courseware['uploads']:
-                    download_list.append(
-                        (upload['name'], ref_url.format(upload['reference_id'])))
+                    if upload['allow_download']:
+                        download_list.append(
+                            (upload['name'], ref_url.format(upload['reference_id'])))
+                    else:
+                        r = s.get(preview_url.format(upload['reference_id']))
+                        file_appendix = re.findall(
+                            "(\.[a-z|A-Z]*)\?", r.json()['url'])
+                        if len(file_appendix) > 0:
+                            download_list.append(
+                                (re.sub(".([a-z|A-Z]*?)$", file_appendix[0], upload['name']), r.json()['url']))
+                        else:
+                            download_list.append(
+                                (upload['name'], r.json()['url']))
     return download_list
 
 
@@ -92,23 +106,33 @@ def parse_content_command(s: str, index_max: int) -> list:
         return list(range(index_max))
 
 
-if __name__ == "__main__":
-    s = requests.session()
-    user_school_id = input("input your zju school id\n")
-    passport.getPass(s, user_school_id)
-    login_xzzd(s)
-    user_id = get_user_id(s)
-    course_list = get_course_list(s, user_id)
+def download_course(s, course_list):
     for index, course in enumerate(course_list):
         print(index, course[0])
-    course_index = int(input("select course with index\n"))
-    download_list = get_download_list(s, course_list[course_index][1])
+    course_index_list = parse_content_command(
+        input("select course with [index1, index2...] or 'all'\n"), len(course_list))
+    for course_index in course_index_list:
+        while download_courseware(s, get_download_list(s, course_list[course_index][1])):
+            print("returning to courseware list")
+    sig = input("return to course list?[y|n]\n")
+    return sig != "n"
+
+
+def download_courseware(s, download_list):
     for index, course_ware in enumerate(download_list):
         print(index, course_ware)
     course_ware_index = parse_content_command(
-        input("select course ware with index list\n"), len(download_list))
-    dir_appendix = input(
-        "input download directory, default current directory\n")
+        input("select courseware with [index1, index2...] or 'all'\n"), len(download_list))
+    if len(sys.argv) > 1:
+        dir_appendix = sys.argv[1]
+        dir_choice = input(
+            "use {} as download path?[y|n]".format(dir_appendix))
+        if dir_choice != "y":
+            dir_appendix = input(
+                "input download directory, default current directory\n")
+    else:
+        dir_appendix = input(
+            "input download directory, default current directory\n")
     file_name = ""
     for index in course_ware_index:
         if not os.path.exists(dir_appendix):
@@ -120,6 +144,19 @@ if __name__ == "__main__":
         try:
             download_file(s, download_list[index][1],
                           file_name)
-            print("download successful")
+            print("download {} successfully".format(download_list[index][0]))
         except:
-            print("download error")
+            print("download {} failed".format(download_list[index][0]))
+    sig = input("return to courseware list?[y|n]\n")
+    return sig != "n"
+
+
+if __name__ == "__main__":
+    s = requests.session()
+    user_school_id = input("input your zju school id\n")
+    passport.getPass(s, user_school_id)
+    login_xzzd(s)
+    user_id = get_user_id(s)
+    course_list = get_course_list(s, user_id)
+    while download_course(s, course_list):
+        print("returning to course list")
